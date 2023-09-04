c9ff674bc3253357ac226cb5b651f0ba2698e359
Mary <mary@mary.zone>
2023-09-04 22:07:03 +0000

    Some updates for inputs

--- src/th06_102h.exe/Global/setButtonFromControllerInputs.c
+++ src/th06_102h.exe/Global/setButtonFromControllerInputs.c
@@ -1,26 +1,30 @@
 
-ushort setButtonFromControllerInputs
-                 (ushort *outButtons,short controllerButtonToTest,ushort touhouButton,
-                 uint inputButtons)
+uint SetButtonFromControllerInputs
+               (ushort *outButtons,short controllerButtonToTest,ushort touhouButton,
+               uint inputButtons)
 
 {
-  ushort uVar1;
-  uint uVar2;
+  uint mask;
+  uint local_10;
+  ushort tmpButton;
   
   if (controllerButtonToTest < 0) {
-    touhouButton = 0;
+    local_10 = 0;
   }
   else {
-    uVar2 = 1 << ((byte)controllerButtonToTest & 0x1f);
-    uVar1 = touhouButton;
-    if ((inputButtons & uVar2) == 0) {
-      uVar1 = 0;
+    mask = 1 << ((byte)controllerButtonToTest & 0x1f);
+    tmpButton = touhouButton;
+    if ((inputButtons & mask) == 0) {
+      tmpButton = 0;
     }
-    *outButtons = *outButtons | uVar1;
-    if ((inputButtons & uVar2) == 0) {
-      touhouButton = 0;
+    *outButtons = *outButtons | tmpButton;
+    if ((inputButtons & mask) == 0) {
+      local_10 = 0;
+    }
+    else {
+      local_10 = (uint)touhouButton;
     }
   }
-  return touhouButton;
+  return local_10;
 }
 

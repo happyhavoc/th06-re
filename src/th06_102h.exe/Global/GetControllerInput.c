f6ba17d0db103531e69c7b841e4dee1aa5a5ac1c
Mary <mary@mary.zone>
2023-09-06 21:43:35 +0000

    More inputs

--- src/th06_102h.exe/Global/GetControllerInput.c
+++ src/th06_102h.exe/Global/GetControllerInput.c
@@ -3,154 +3,146 @@ ushort GetControllerInput(ushort buttons)
 
 {
   MMRESULT MVar1;
-  DWORD DVar2;
-  uint uVar3;
-  HRESULT HVar4;
-  int iVar5;
-  uint uVar6;
-  joyinfoex_tag *pjVar7;
-  DIJOYSTATE2 *pDVar8;
+  DWORD shootState;
+  uint uVar2;
+  HRESULT stateHr;
+  int iVar3;
+  uint uVar4;
+  joyinfoex_tag *pjVar5;
+  DIJOYSTATE2 *pDVar6;
   uint unaff_retaddr;
   int local_160;
-  DIJOYSTATE2 local_15c;
+  DIJOYSTATE2 js;
   uint local_4c;
-  HRESULT local_48;
-  joyinfoex_tag joyconPos;
+  HRESULT hr;
+  joyinfoex_tag pji;
   
   local_4c = __security_cookie ^ unaff_retaddr;
   if (g_GameContext.controller == (LPDIRECTINPUTDEVICE8A)0x0) {
-    pjVar7 = &joyconPos;
-    for (iVar5 = 0xd; iVar5 != 0; iVar5 = iVar5 + -1) {
-      pjVar7->dwSize = 0;
-      pjVar7 = (joyinfoex_tag *)&pjVar7->dwFlags;
+    pjVar5 = &pji;
+    for (iVar3 = 0xd; iVar3 != 0; iVar3 = iVar3 + -1) {
+      pjVar5->dwSize = 0;
+      pjVar5 = (joyinfoex_tag *)&pjVar5->dwFlags;
     }
-    joyconPos.dwSize = 0x34;
-    joyconPos.dwFlags = 0xff;
-    MVar1 = joyGetPosEx(0,&joyconPos);
+    pji.dwSize = 0x34;
+    pji.dwFlags = 0xff;
+    MVar1 = joyGetPosEx(0,&pji);
     if (MVar1 == 0) {
-      DVar2 = SetButtonFromControllerInputs
-                        (&buttons,g_ControllerMapping.shootButton,SHOOT,joyconPos.dwButtons);
+      shootState = SetButtonFromControllerInputs
+                             (&buttons,g_ControllerMapping.shootButton,SHOOT,pji.dwButtons);
       if (g_ControllerMapping.shootButton == g_ControllerMapping.focusButton) {
-        if (DVar2 == 0) {
-          if (DAT_0069d8f4 < 9) {
-            DAT_0069d8f4 = 0;
+        if (shootState == 0) {
+          if (g_FocusButtonConflictState < 9) {
+            g_FocusButtonConflictState = 0;
           }
           else {
-            DAT_0069d8f4 = DAT_0069d8f4 - 8;
+            g_FocusButtonConflictState = g_FocusButtonConflictState - 8;
           }
         }
         else {
-          if (DAT_0069d8f4 < 0x10) {
-            DAT_0069d8f4 = DAT_0069d8f4 + 1;
+          if (g_FocusButtonConflictState < 0x10) {
+            g_FocusButtonConflictState = g_FocusButtonConflictState + 1;
           }
-          if (7 < DAT_0069d8f4) {
+          if (7 < g_FocusButtonConflictState) {
             buttons = buttons | 4;
           }
         }
       }
       else {
-        SetButtonFromControllerInputs
-                  (&buttons,g_ControllerMapping.focusButton,FOCUS,joyconPos.dwButtons);
+        SetButtonFromControllerInputs(&buttons,g_ControllerMapping.focusButton,FOCUS,pji.dwButtons);
       }
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.bombButton,BOMB,joyconPos.dwButtons);
+                (&buttons,g_GameContext.cfg.controllerMapping.bombButton,BOMB,pji.dwButtons);
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.menuButton,MENU,joyconPos.dwButtons);
+                (&buttons,g_GameContext.cfg.controllerMapping.menuButton,MENU,pji.dwButtons);
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.upButton,UP,joyconPos.dwButtons);
+                (&buttons,g_GameContext.cfg.controllerMapping.upButton,UP,pji.dwButtons);
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.downButton,DOWN,joyconPos.dwButtons);
+                (&buttons,g_GameContext.cfg.controllerMapping.downButton,DOWN,pji.dwButtons);
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.leftButton,LEFT,joyconPos.dwButtons);
+                (&buttons,g_GameContext.cfg.controllerMapping.leftButton,LEFT,pji.dwButtons);
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.rightButton,RIGHT,joyconPos.dwButtons)
-      ;
+                (&buttons,g_GameContext.cfg.controllerMapping.rightButton,RIGHT,pji.dwButtons);
       SetButtonFromControllerInputs
-                (&buttons,g_GameContext.cfg.controllerMapping.unkButton,0x100,joyconPos.dwButtons);
-      uVar3 = JOYSTICK_CAPS.wXmax - JOYSTICK_CAPS.wXmin >> 2;
-      uVar6 = JOYSTICK_CAPS.wYmax - JOYSTICK_CAPS.wYmin >> 2;
-      buttons = buttons | -(ushort)((JOYSTICK_CAPS.wXmin + JOYSTICK_CAPS.wXmax >> 1) + uVar3 <
-                                   joyconPos.dwXpos) & RIGHT |
-                -(ushort)(joyconPos.dwXpos <
-                         (JOYSTICK_CAPS.wXmin + JOYSTICK_CAPS.wXmax >> 1) - uVar3) & LEFT |
-                -(ushort)((JOYSTICK_CAPS.wYmin + JOYSTICK_CAPS.wYmax >> 1) + uVar6 <
-                         joyconPos.dwYpos) & DOWN |
-                -(ushort)(joyconPos.dwYpos <
-                         (JOYSTICK_CAPS.wYmin + JOYSTICK_CAPS.wYmax >> 1) - uVar6) & UP;
+                (&buttons,g_GameContext.cfg.controllerMapping.unkButton,0x100,pji.dwButtons);
+      uVar2 = g_JoystickCaps.wXmax - g_JoystickCaps.wXmin >> 2;
+      uVar4 = g_JoystickCaps.wYmax - g_JoystickCaps.wYmin >> 2;
+      buttons = buttons | -(ushort)((g_JoystickCaps.wXmin + g_JoystickCaps.wXmax >> 1) + uVar2 <
+                                   pji.dwXpos) & RIGHT |
+                -(ushort)(pji.dwXpos < (g_JoystickCaps.wXmin + g_JoystickCaps.wXmax >> 1) - uVar2) &
+                LEFT | -(ushort)((g_JoystickCaps.wYmin + g_JoystickCaps.wYmax >> 1) + uVar4 <
+                                pji.dwYpos) & DOWN |
+                -(ushort)(pji.dwYpos < (g_JoystickCaps.wYmin + g_JoystickCaps.wYmax >> 1) - uVar4) &
+                UP;
     }
   }
   else {
-    HVar4 = (*(g_GameContext.controller)->lpVtbl->Poll)(g_GameContext.controller);
-    if (HVar4 < 0) {
+    stateHr = (*(g_GameContext.controller)->lpVtbl->Poll)(g_GameContext.controller);
+    if (stateHr < 0) {
       local_160 = 0;
       DebugPrint2("error : DIERR_INPUTLOST\n");
-      local_48 = (*(g_GameContext.controller)->lpVtbl->Acquire)(g_GameContext.controller);
+      hr = (*(g_GameContext.controller)->lpVtbl->Acquire)(g_GameContext.controller);
       do {
-        if (local_48 != -0x7ff8ffe2) break;
-        local_48 = (*(g_GameContext.controller)->lpVtbl->Acquire)(g_GameContext.controller);
+        if (hr != -0x7ff8ffe2) break;
+        hr = (*(g_GameContext.controller)->lpVtbl->Acquire)(g_GameContext.controller);
         DebugPrint2("error : DIERR_INPUTLOST %d\n",local_160);
         local_160 = local_160 + 1;
       } while (local_160 < 400);
     }
     else {
-      pDVar8 = &local_15c;
-      for (iVar5 = 0x44; iVar5 != 0; iVar5 = iVar5 + -1) {
-        pDVar8->lX = 0;
-        pDVar8 = (DIJOYSTATE2 *)&pDVar8->lY;
+      pDVar6 = &js;
+      for (iVar3 = 0x44; iVar3 != 0; iVar3 = iVar3 + -1) {
+        pDVar6->lX = 0;
+        pDVar6 = (DIJOYSTATE2 *)&pDVar6->lY;
       }
-      HVar4 = (*(g_GameContext.controller)->lpVtbl->GetDeviceState)
-                        (g_GameContext.controller,0x110,&local_15c);
-      if (-1 < HVar4) {
-        DVar2 = SetButtonFromDirectInputJoystate
-                          (&buttons,g_GameContext.cfg.controllerMapping.shootButton,1,
-                           local_15c.rgbButtons);
+      stateHr = (*(g_GameContext.controller)->lpVtbl->GetDeviceState)
+                          (g_GameContext.controller,0x110,&js);
+      if (-1 < stateHr) {
+        shootState = SetButtonFromDirectInputJoystate
+                               (&buttons,g_GameContext.cfg.controllerMapping.shootButton,1,
+                                js.rgbButtons);
         if (g_ControllerMapping.shootButton == g_ControllerMapping.focusButton) {
-          if (DVar2 == 0) {
-            if (DAT_0069d8f4 < 9) {
-              DAT_0069d8f4 = 0;
+          if (shootState == 0) {
+            if (g_FocusButtonConflictState < 9) {
+              g_FocusButtonConflictState = 0;
             }
             else {
-              DAT_0069d8f4 = DAT_0069d8f4 - 8;
+              g_FocusButtonConflictState = g_FocusButtonConflictState - 8;
             }
           }
           else {
-            if (DAT_0069d8f4 < 0x10) {
-              DAT_0069d8f4 = DAT_0069d8f4 + 1;
+            if (g_FocusButtonConflictState < 0x10) {
+              g_FocusButtonConflictState = g_FocusButtonConflictState + 1;
             }
-            if (7 < DAT_0069d8f4) {
+            if (7 < g_FocusButtonConflictState) {
               buttons = buttons | 4;
             }
           }
         }
         else {
           SetButtonFromDirectInputJoystate
-                    (&buttons,g_GameContext.cfg.controllerMapping.focusButton,4,local_15c.rgbButtons
-                    );
+                    (&buttons,g_GameContext.cfg.controllerMapping.focusButton,4,js.rgbButtons);
         }
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.bombButton,2,local_15c.rgbButtons);
+                  (&buttons,g_GameContext.cfg.controllerMapping.bombButton,2,js.rgbButtons);
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.menuButton,8,local_15c.rgbButtons);
+                  (&buttons,g_GameContext.cfg.controllerMapping.menuButton,8,js.rgbButtons);
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.upButton,0x10,local_15c.rgbButtons);
+                  (&buttons,g_GameContext.cfg.controllerMapping.upButton,0x10,js.rgbButtons);
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.downButton,0x20,local_15c.rgbButtons
-                  );
+                  (&buttons,g_GameContext.cfg.controllerMapping.downButton,0x20,js.rgbButtons);
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.leftButton,0x40,local_15c.rgbButtons
-                  );
+                  (&buttons,g_GameContext.cfg.controllerMapping.leftButton,0x40,js.rgbButtons);
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.rightButton,0x80,
-                   local_15c.rgbButtons);
+                  (&buttons,g_GameContext.cfg.controllerMapping.rightButton,0x80,js.rgbButtons);
         SetButtonFromDirectInputJoystate
-                  (&buttons,g_GameContext.cfg.controllerMapping.unkButton,0x100,local_15c.rgbButtons
-                  );
-        buttons = buttons | (local_15c.lX <= (short)g_GameContext.cfg.padAxisX) - 1 & 0x80 |
-                  (SBORROW4(local_15c.lX,-(int)(short)g_GameContext.cfg.padAxisX) ==
-                  local_15c.lX + (short)g_GameContext.cfg.padAxisX < 0) - 1 & 0x40 |
-                  (local_15c.lY <= (short)g_GameContext.cfg.padAxisY) - 1 & 0x20 |
-                  (SBORROW4(local_15c.lY,-(int)(short)g_GameContext.cfg.padAxisY) ==
-                  local_15c.lY + (short)g_GameContext.cfg.padAxisY < 0) - 1 & 0x10;
+                  (&buttons,g_GameContext.cfg.controllerMapping.unkButton,0x100,js.rgbButtons);
+        buttons = buttons | (js.lX <= (short)g_GameContext.cfg.padAxisX) - 1 & 0x80 |
+                  (SBORROW4(js.lX,-(int)(short)g_GameContext.cfg.padAxisX) ==
+                  js.lX + (short)g_GameContext.cfg.padAxisX < 0) - 1 & 0x40 |
+                  (js.lY <= (short)g_GameContext.cfg.padAxisY) - 1 & 0x20 |
+                  (SBORROW4(js.lY,-(int)(short)g_GameContext.cfg.padAxisY) ==
+                  js.lY + (short)g_GameContext.cfg.padAxisY < 0) - 1 & 0x10;
       }
     }
   }
--- src/th06_102h.exe/Global/GetJoystickCaps.c
+++ src/th06_102h.exe/Global/GetJoystickCaps.c
@@ -9,11 +9,11 @@ bool GetJoystickCaps(void)
   joyinfo.dwFlags = 0xff;
   MVar1 = joyGetPosEx(0,&joyinfo);
   if (MVar1 == 0) {
-    joyGetDevCapsA(0,&JOYSTICK_CAPS,0x194);
+    joyGetDevCapsA(0,&g_JoystickCaps,0x194);
   }
   else {
     GameErrorContextLog(&g_GameErrorContext,
-                        "使えるパッドが存在しないようです、残念\n");
+                        "使えるパッドが存在しないよ��です、残念\n");
   }
   return MVar1 != 0;
 }

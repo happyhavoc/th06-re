4648b2ee4d0661701978c131c454091148eb5f55
roblabla <unfiltered@roblab.la>
2023-09-16 13:30:27 +0000

    Oops

--- src/th06_102h.exe/AsciiManager/RegisterChain.c
+++ src/th06_102h.exe/AsciiManager/RegisterChain.c
@@ -6,11 +6,11 @@ int AsciiManager::RegisterChain(void)
 {
   int iVar1;
   
-  g_AsciiManagerChain._0_4_ = FUN_00401140;
-  g_AsciiManagerChain.callback = AddedCallback;
-  g_AsciiManagerChain.addedCallback = DeletedCallback;
-  g_AsciiManagerChain.unkPtr = (ChainElem *)&g_AsciiManager;
-  iVar1 = Chain::AddToCalcChain((Chain *)&DAT_0069d918,(ChainElem *)&DAT_00487aac,1);
+  ChainElem_00487aac.callback = FUN_00401140;
+  ChainElem_00487aac.addedCallback = AddedCallback;
+  ChainElem_00487aac.deletedCallback = DeletedCallback;
+  ChainElem_00487aac.arg = &g_AsciiManager;
+  iVar1 = Chain::AddToCalcChain((Chain *)&DAT_0069d918,&ChainElem_00487aac,1);
   if (iVar1 == 0) {
     ChainElem_0047b8e0.callback = FUN_00401250;
     ChainElem_0047b8e0.addedCallback = 0;
@@ -21,7 +21,7 @@ int AsciiManager::RegisterChain(void)
     _DAT_00487ad4 = 0;
     _DAT_00487ad8 = 0;
     _DAT_00487ae8 = &g_AsciiManager;
-    Chain::AddToDrawChain((Chain *)&DAT_0069d918,(ChainElem *)&g_AsciiManagerChain.arg,10);
+    Chain::AddToDrawChain((Chain *)&DAT_0069d918,(ChainElem *)&DAT_00487acc,10);
     iVar1 = 0;
   }
   else {

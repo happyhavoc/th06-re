af329e365bdd4af8245c0a5a2b100ebce4d6c330
PieVieRo <tymek24m@gmail.com>
2024-03-22 18:54:53 +0000

    Generic reverse engineering progress

--- src/th06_102h.exe/MainMenu/beginStartup.c
+++ src/th06_102h.exe/MainMenu/beginStartup.c
@@ -1,5 +1,5 @@
 
-ZunResult __fastcall MainMenu::beginStartup(MainMenu *menu)
+ZunResult __thiscall MainMenu::beginStartup(MainMenu *this)
 
 {
   D3DXVECTOR3 *pDVar1;
@@ -8,7 +8,7 @@ ZunResult __fastcall MainMenu::beginStartup(MainMenu *menu)
   int i;
   DWORD time;
   
-  loadTitleAnm = MainMenu::loadTitleAnm(menu);
+  loadTitleAnm = MainMenu::loadTitleAnm(this);
   if (loadTitleAnm == ZUN_SUCCESS) {
     if (g_Supervisor.startup_time_for_menu_music != 0) {
       time = timeGetTime();
@@ -19,20 +19,20 @@ ZunResult __fastcall MainMenu::beginStartup(MainMenu *menu)
       Supervisor::PlayAudio("bgm/th06_01.mid");
     }
     for (i = 0; i < 122; i = i + 1) {
-      (&menu->vm1)[i].pendingInterrupt = 1;
-      *(uint *)&(&menu->vm1)[i].flags = *(uint *)&(&menu->vm1)[i].flags | 8;
+      (&this->vm1)[i].pendingInterrupt = 1;
+      *(uint *)&(&this->vm1)[i].flags = *(uint *)&(&this->vm1)[i].flags | 8;
       if ((g_Supervisor.cfg.opts & 1) == 0) {
-        (&menu->vm1)[i].color.color = 0xff000000;
+        (&this->vm1)[i].color.color = 0xff000000;
       }
       else {
-        (&menu->vm1)[i].color.color = 0xffffffff;
+        (&this->vm1)[i].color.color = 0xffffffff;
       }
-      pDVar1 = &(&menu->vm1)[i].pos2;
+      pDVar1 = &(&this->vm1)[i].pos2;
       pDVar1->x = 0.0;
       pDVar1->y = 0.0;
       pDVar1->z = 0.0;
     }
-    menu->gameState = STATE_PRE_INPUT;
+    this->gameState = STATE_PRE_INPUT;
     result = ZUN_SUCCESS;
   }
   else {

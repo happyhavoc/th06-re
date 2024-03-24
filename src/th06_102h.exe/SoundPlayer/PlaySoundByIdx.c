b7aff084fe23fef1c4cfa427bbce1a87b8fe5116
PieVieRo <tymek24m@gmail.com>
2024-03-24 15:28:17 +0000

    PlaySoundByIdx probably shouldnt return anything

--- src/th06_102h.exe/SoundPlayer/PlaySoundByIdx.c
+++ src/th06_102h.exe/SoundPlayer/PlaySoundByIdx.c
@@ -1,26 +1,21 @@
 
-SoundPlayer * __thiscall SoundPlayer::PlaySoundByIdx(SoundPlayer *this,int soundIdx,int unused)
+void __thiscall SoundPlayer::PlaySoundByIdx(SoundPlayer *this,int soundIdx,int unused)
 
 {
-  SoundPlayer *pSVar1;
-  SoundPlayer *local_8;
+  int local_8;
   short SFXToPlay;
   
   SFXToPlay = SOUND_BUFFER_IDX_VOL[soundIdx].unk;
-  pSVar1 = (SoundPlayer *)soundIdx;
-  for (local_8 = (SoundPlayer *)0x0;
-      ((int)local_8 < 3 && (pSVar1 = local_8, -1 < this->soundBuffersToPlay[(int)local_8]));
-      local_8 = (SoundPlayer *)((int)&(local_8->csoundmanager).m_pDS + 1)) {
-    if (this->soundBuffersToPlay[(int)local_8] == soundIdx) {
-      return this;
+  for (local_8 = 0; (local_8 < 3 && (-1 < this->soundBuffersToPlay[local_8])); local_8 = local_8 + 1
+      ) {
+    if (this->soundBuffersToPlay[local_8] == soundIdx) {
+      return;
     }
-    pSVar1 = this;
   }
-  if ((int)local_8 < 3) {
-    this->soundBuffersToPlay[(int)local_8] = soundIdx;
+  if (local_8 < 3) {
+    this->soundBuffersToPlay[local_8] = soundIdx;
     this->unk_408[soundIdx] = (int)SFXToPlay;
-    pSVar1 = this;
   }
-  return pSVar1;
+  return;
 }
 

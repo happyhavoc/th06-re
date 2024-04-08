ceca9d91939d6782b57081639ac940ac5c192ba8
Renzo904 <63516530+Renzo904@users.noreply.github.com>
2024-04-08 04:32:57 +0000

    Proper naming for Supervisor::TickTimer parameters

--- src/th06_102h.exe/Supervisor/TickTimer.c
+++ src/th06_102h.exe/Supervisor/TickTimer.c
@@ -1,15 +1,15 @@
 
-void __thiscall Supervisor::TickTimer(Supervisor *this,int *param_1,float *param_2)
+void __thiscall Supervisor::TickTimer(Supervisor *this,int *frames,float *subframes)
 
 {
   if (this->framerateMultiplier < 0.99 == (this->framerateMultiplier == 0.99)) {
-    *param_1 = *param_1 + 1;
+    *frames = *frames + 1;
   }
   else {
-    *param_2 = *param_2 + this->effectiveFramerateMultiplier;
-    if (1.0 <= *param_2) {
-      *param_1 = *param_1 + 1;
-      *param_2 = *param_2 - 1.0;
+    *subframes = *subframes + this->effectiveFramerateMultiplier;
+    if (1.0 <= *subframes) {
+      *frames = *frames + 1;
+      *subframes = *subframes - 1.0;
     }
   }
   return;

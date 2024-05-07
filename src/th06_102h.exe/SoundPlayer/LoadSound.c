4a3e748ba8264afc161db42e6bc55294ebd201ed
PieVieRo <tymek24m@gmail.com>
2024-05-07 16:16:54 +0000

    Generic reverse engineering progress

--- src/th06_102h.exe/SoundPlayer/LoadSound.c
+++ src/th06_102h.exe/SoundPlayer/LoadSound.c
@@ -2,23 +2,24 @@
 ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path)
 
 {
-  ZunResult ZVar1;
+  WORD WVar1;
+  ZunResult ZVar2;
   char *soundFileData;
   int compared;
   HRESULT HVar3;
   int i;
   uint uVar4;
   WAVEFORMATEX *piVar1;
+  WAVEFORMATEX *puVar5;
   DSBUFFERDESC *pDVar6;
-  undefined4 *piVar2;
-  undefined4 *puVar7;
+  WAVEFORMATEX *piVar2;
   WAVEFORMATEX wavData;
   char *soundFileData2;
-  LPVOID AudioPtr1;
+  WAVEFORMATEX *AudioPtr1;
   dword AudioBytes1;
   DWORD AudioBytes2;
-  LPVOID AudioPtr2;
-  DWORD formatSize;
+  WAVEFORMATEX *AudioPtr2;
+  int formatSize;
   WAVEFORMATEX *wavDataPtr;
   DSBUFFERDESC dsBuffer;
   uint stackCookie;
@@ -28,7 +29,7 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
   
   stackCookie = __security_cookie ^ unaff_retaddr;
   if (this->csoundmanager_ptr == (CSoundManager *)0x0) {
-    ZVar1 = ZUN_SUCCESS;
+    ZVar2 = ZUN_SUCCESS;
   }
   else {
     if (this->sound_buffers[idx] != (LPDIRECTSOUNDBUFFER)0x0) {
@@ -38,7 +39,7 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
     soundFileData = (char *)FileSystem::OpenPath(path,0);
     sFD_Cursor = soundFileData;
     if (soundFileData == (char *)0x0) {
-      ZVar1 = ZUN_ERROR;
+      ZVar2 = ZUN_ERROR;
     }
     else {
       compared = _strncmp(soundFileData,"RIFF",4);
@@ -48,11 +49,11 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
         compared = _strncmp(sFD_Cursor,"WAVE",4);
         if (compared == 0) {
           sFD_Cursor = sFD_Cursor + 4;
-          wavDataPtr = GetWavFormatData(sFD_Cursor,"fmt ",(int *)&formatSize,fileSize + -12);
+          wavDataPtr = GetWavFormatData(sFD_Cursor,"fmt ",&formatSize,fileSize + -12);
           if (wavDataPtr == (WAVEFORMATEX *)0x0) {
             GameErrorContextLog(&g_GameErrorContext,"Wav ファイルじゃない? %s\n",path);
             _free(soundFileData);
-            ZVar1 = ZUN_ERROR;
+            ZVar2 = ZUN_ERROR;
           }
           else {
             wavData.wFormatTag = wavDataPtr->wFormatTag;
@@ -62,11 +63,11 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
             wavData.nBlockAlign = wavDataPtr->nBlockAlign;
             wavData.wBitsPerSample = wavDataPtr->wBitsPerSample;
             wavData.cbSize = wavDataPtr->cbSize;
-            wavDataPtr = GetWavFormatData(sFD_Cursor,"data",(int *)&formatSize,fileSize + -0xc);
+            wavDataPtr = GetWavFormatData(sFD_Cursor,"data",&formatSize,fileSize + -0xc);
             if (wavDataPtr == (WAVEFORMATEX *)0x0) {
               GameErrorContextLog(&g_GameErrorContext,"Wav ファイルじゃない? %s\n",path);
               _free(soundFileData);
-              ZVar1 = ZUN_ERROR;
+              ZVar2 = ZUN_ERROR;
             }
             else {
               pDVar6 = &dsBuffer;
@@ -83,7 +84,7 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
                                  (LPUNKNOWN)0x0);
               if (HVar3 < 0) {
                 _free(soundFileData);
-                ZVar1 = ZUN_ERROR;
+                ZVar2 = ZUN_ERROR;
               }
               else {
                 HVar3 = (*this->sound_buffers[idx]->lpVtbl->Lock)
@@ -91,39 +92,43 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
                                    &AudioPtr2,&AudioBytes2,0);
                 if (HVar3 < 0) {
                   _free(soundFileData);
-                  ZVar1 = ZUN_ERROR;
+                  ZVar2 = ZUN_ERROR;
                 }
                 else {
                   piVar1 = wavDataPtr;
-                  piVar2 = (undefined4 *)AudioPtr1;
+                  piVar2 = AudioPtr1;
                   for (uVar4 = AudioBytes1 >> 2; uVar4 != 0; uVar4 = uVar4 - 1) {
-                    *piVar2 = *(undefined4 *)piVar1;
+                    WVar1 = piVar1->nChannels;
+                    piVar2->wFormatTag = piVar1->wFormatTag;
+                    piVar2->nChannels = WVar1;
                     piVar1 = (WAVEFORMATEX *)&piVar1->nSamplesPerSec;
-                    piVar2 = piVar2 + 1;
+                    piVar2 = (WAVEFORMATEX *)&piVar2->nSamplesPerSec;
                   }
                   for (uVar4 = AudioBytes1 & 3; uVar4 != 0; uVar4 = uVar4 - 1) {
-                    *(undefined *)piVar2 = *(undefined *)&piVar1->wFormatTag;
+                    *(undefined *)&piVar2->wFormatTag = *(undefined *)&piVar1->wFormatTag;
                     piVar1 = (WAVEFORMATEX *)((int)&piVar1->wFormatTag + 1);
-                    piVar2 = (undefined4 *)((int)piVar2 + 1);
+                    piVar2 = (WAVEFORMATEX *)((int)&piVar2->wFormatTag + 1);
                   }
                   if (AudioBytes2 != 0) {
-                    piVar2 = (undefined4 *)((int)&wavDataPtr->wFormatTag + AudioBytes1);
-                    puVar7 = (undefined4 *)AudioPtr2;
+                    puVar5 = (WAVEFORMATEX *)((int)&wavDataPtr->wFormatTag + AudioBytes1);
+                    piVar1 = AudioPtr2;
                     for (uVar4 = AudioBytes2 >> 2; uVar4 != 0; uVar4 = uVar4 - 1) {
-                      *puVar7 = *piVar2;
-                      piVar2 = piVar2 + 1;
-                      puVar7 = puVar7 + 1;
+                      WVar1 = puVar5->nChannels;
+                      piVar1->wFormatTag = puVar5->wFormatTag;
+                      piVar1->nChannels = WVar1;
+                      puVar5 = (WAVEFORMATEX *)&puVar5->nSamplesPerSec;
+                      piVar1 = (WAVEFORMATEX *)&piVar1->nSamplesPerSec;
                     }
                     for (uVar4 = AudioBytes2 & 3; uVar4 != 0; uVar4 = uVar4 - 1) {
-                      *(undefined *)puVar7 = *(undefined *)piVar2;
-                      piVar2 = (undefined4 *)((int)piVar2 + 1);
-                      puVar7 = (undefined4 *)((int)puVar7 + 1);
+                      *(undefined *)&piVar1->wFormatTag = *(undefined *)&puVar5->wFormatTag;
+                      puVar5 = (WAVEFORMATEX *)((int)&puVar5->wFormatTag + 1);
+                      piVar1 = (WAVEFORMATEX *)((int)&piVar1->wFormatTag + 1);
                     }
                   }
                   (*this->sound_buffers[idx]->lpVtbl->Unlock)
                             (this->sound_buffers[idx],AudioPtr1,AudioBytes1,AudioPtr2,AudioBytes2);
                   _free(soundFileData);
-                  ZVar1 = ZUN_SUCCESS;
+                  ZVar2 = ZUN_SUCCESS;
                 }
               }
             }
@@ -132,17 +137,17 @@ ZunResult __thiscall SoundPlayer::LoadSound(SoundPlayer *this,int idx,char *path
         else {
           GameErrorContextLog(&g_GameErrorContext,"Wav ファイルじゃない? %s\n",path);
           _free(soundFileData);
-          ZVar1 = ZUN_ERROR;
+          ZVar2 = ZUN_ERROR;
         }
       }
       else {
         GameErrorContextLog(&g_GameErrorContext,"Wav ファイルじゃない %s\n",path);
         _free(soundFileData);
-        ZVar1 = ZUN_ERROR;
+        ZVar2 = ZUN_ERROR;
       }
     }
   }
   __security_check_cookie(stackCookie ^ unaff_retaddr);
-  return ZVar1;
+  return ZVar2;
 }
 

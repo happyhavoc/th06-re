4f76a6add740725ad6e016bef0ae10c9cc1b754c
PieVieRo <tymek24m@gmail.com>
2024-05-09 18:40:20 +0000

    tiny fix

--- src/th06_102h.exe/EffectManager/AddedCallback.c
+++ src/th06_102h.exe/EffectManager/AddedCallback.c
@@ -1,7 +1,7 @@
 
 /* WARNING: Unknown calling convention -- yet parameter storage is locked */
 
-int EffectManager::AddedCallback(EffectManager *param_1)
+ZunResult EffectManager::AddedCallback(EffectManager *param_1)
 
 {
   ZunResult ZVar1;
@@ -12,45 +12,45 @@ int EffectManager::AddedCallback(EffectManager *param_1)
   case 1:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff01.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
     break;
   case 2:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff02.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
     break;
   case 3:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff03.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
     break;
   case 4:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff04.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
     break;
   case 5:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff05.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
     break;
   case 6:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff05.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
     break;
   case 7:
     ZVar1 = AnmManager::LoadAnm(g_AnmManager,0xb,"data/eff04.anm",0x2b3);
     if (ZVar1 != ZUN_SUCCESS) {
-      return -1;
+      return ZUN_ERROR;
     }
   }
-  return 0;
+  return ZUN_SUCCESS;
 }
 

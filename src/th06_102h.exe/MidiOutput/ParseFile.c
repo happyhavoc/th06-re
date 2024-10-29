31a93842d7b2c57be5b43d5fb8df767b0ec4c45b
Mary <mary@mary.zone>
2024-10-29 12:06:11 +0000

    Map more stack slot for ParseFile

--- src/th06_102h.exe/MidiOutput/ParseFile.c
+++ src/th06_102h.exe/MidiOutput/ParseFile.c
@@ -13,18 +13,18 @@ ZunResult __thiscall th06::MidiOutput::ParseFile(MidiOutput *this,int fileIdx)
   int iVar3;
   undefined4 *puVar4;
   undefined4 *puVar5;
-  ushort uStackY_58;
+  ushort tmp_reg;
   undefined local_3c;
   undefined uStack_3b;
   undefined uStack_3a;
   undefined uStack_39;
   size_t trackLength;
-  int hdrLenLe;
-  undefined local_20;
+  int hdrLengthLE;
+  undefined hdrLengthBE;
   undefined uStack_1f;
   undefined uStack_1e;
   undefined uStack_1d;
-  undefined *endOfHeader;
+  undefined *currentCursor;
   int trackIdx;
   byte *file_data;
   undefined2 format;
@@ -47,10 +47,10 @@ ZunResult __thiscall th06::MidiOutput::ParseFile(MidiOutput *this,int fileIdx)
     uStack_1d = (undefined)((uint)hdrLenBe >> 0x18);
     uStack_1e = (undefined)((uint)hdrLenBe >> 0x10);
     uStack_1f = (undefined)((uint)hdrLenBe >> 8);
-    local_20 = (undefined)hdrLenBe;
-    hdrLenLe = CONCAT13(local_20,CONCAT12(uStack_1f,CONCAT11(uStack_1e,uStack_1d)));
+    hdrLengthBE = (undefined)hdrLenBe;
+    hdrLengthLE = CONCAT13(hdrLengthBE,CONCAT12(uStack_1f,CONCAT11(uStack_1e,uStack_1d)));
                     /* Get a pointer to the end of the header chunk */
-    endOfHeader = (undefined *)((int)(file_data + 8) + hdrLenLe);
+    currentCursor = (undefined *)((int)(file_data + 8) + hdrLengthLE);
                     /* Read the format. Only three values of format are specified:
                        0: the file contains a single multi-channel track
                        1: the file contains one or more simultaneous tracks (or MIDI outputs) of a
@@ -58,17 +58,17 @@ ZunResult __thiscall th06::MidiOutput::ParseFile(MidiOutput *this,int fileIdx)
                        2: the file contains one or more sequentially independent single-track
                        patterns */
     format = *(undefined2 *)(file_data + 8);
-    uStackY_58 = CONCAT11((char)format,(char)((ushort)format >> 8));
-    this->format = (uint)uStackY_58;
+    tmp_reg = CONCAT11((char)format,(char)((ushort)format >> 8));
+    this->format = (uint)tmp_reg;
                     /* Read the number of tracks in this midi file.  */
-    uStackY_58 = CONCAT11((char)*(undefined2 *)(file_data + 0xc),
-                          (char)((ushort)*(undefined2 *)(file_data + 0xc) >> 8));
-    this->divisons = (uint)uStackY_58;
+    tmp_reg = CONCAT11((char)*(undefined2 *)(file_data + 0xc),
+                       (char)((ushort)*(undefined2 *)(file_data + 0xc) >> 8));
+    this->divisons = (uint)tmp_reg;
                     /* Read the divisions in this track. Note that this doesn't appear to support
                        "negative SMPTE format", which happens when the MSB is set. */
-    uStackY_58 = CONCAT11((char)*(undefined2 *)(file_data + 10),
-                          (char)((ushort)*(undefined2 *)(file_data + 10) >> 8));
-    this->num_tracks = (uint)uStackY_58;
+    tmp_reg = CONCAT11((char)*(undefined2 *)(file_data + 10),
+                       (char)((ushort)*(undefined2 *)(file_data + 10) >> 8));
+    this->num_tracks = (uint)tmp_reg;
                     /* Allocate this->divisions * 32 bytes. */
     pMVar1 = (MidiTrack *)_malloc(this->num_tracks << 5);
     this->tracks = pMVar1;
@@ -88,7 +88,7 @@ ZunResult __thiscall th06::MidiOutput::ParseFile(MidiOutput *this,int fileIdx)
                        
                        First, read the length of the chunk
                         */
-      hdrLenBe = *(undefined4 *)(endOfHeader + 4);
+      hdrLenBe = *(undefined4 *)(currentCursor + 4);
       uStack_39 = (undefined)((uint)hdrLenBe >> 0x18);
       uStack_3a = (undefined)((uint)hdrLenBe >> 0x10);
       uStack_3b = (undefined)((uint)hdrLenBe >> 8);
@@ -99,7 +99,7 @@ ZunResult __thiscall th06::MidiOutput::ParseFile(MidiOutput *this,int fileIdx)
       trackData = _malloc(trackLength);
       this->tracks[trackIdx].trackData = trackData;
       this->tracks[trackIdx].trackPlaying = 1;
-      puVar4 = (undefined4 *)(endOfHeader + 8);
+      puVar4 = (undefined4 *)(currentCursor + 8);
       puVar5 = (undefined4 *)this->tracks[trackIdx].trackData;
                     /* memcpy(this->tracks[trackIdx].trackData, endOfHeader + 8,
                        this->tracks[curDivision].trackLength); */
@@ -113,7 +113,7 @@ ZunResult __thiscall th06::MidiOutput::ParseFile(MidiOutput *this,int fileIdx)
         puVar4 = (undefined4 *)((int)puVar4 + 1);
         puVar5 = (undefined4 *)((int)puVar5 + 1);
       }
-      endOfHeader = (undefined *)((int)(endOfHeader + 8) + trackLength);
+      currentCursor = (undefined *)((int)(currentCursor + 8) + trackLength);
     }
     this->unk120 = 1000000;
     result = ZUN_SUCCESS;
